e7059fbc4982fa04752fb65d455cb951f325156a
vrasmus <rv@rasmusv.com>
2017-05-11 14:03:04 +0200

    receiver test side + data

---
+++ nesC/TestReceive/src/TestReceive.h
@@ -0,0 +1,24 @@
+#ifndef TEST_RECEIVE_H
+#define TEST_RECEIVE_H
+
+#define TOSH_DATA_LENGTH 103
+
+enum{
+	//CC2420_DEF_CHANNEL = 25,
+	TIMER_PERIOD_MILLI = 500,
+	AM_BLINKTORADIO = 6,
+	AM_TEST_SERIAL_MSG = 0x89,
+	SERIAL_PERIOD_MILLI = 10000,
+};
+
+typedef nx_struct TestMsg {
+	nx_uint16_t nodeid;
+	nx_uint8_t counter;
+} TestMsg;
+
+typedef nx_struct serialMsg {
+  nx_uint16_t count;
+  nx_uint8_t payloadLen;
+} serialMsg;
+
+#endif /* TEST_RECEIVE_H */
